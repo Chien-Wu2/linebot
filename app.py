6382c7cd24c92727dda8db14cf76e2df64416826
Chien-Wu2/ <g0910186@hchs.hc.edu.t>
2021-07-10 14:07:50 +0800

    吃飽沒

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+ from flask import Flask, request, abort
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,StickerSendMessage
 )
 
 app = Flask(__name__)
@@ -37,9 +37,18 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = event.message.text
+
+    if '吃飽' in msg:
+        r = '吃飽了'
+    
+
+
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
