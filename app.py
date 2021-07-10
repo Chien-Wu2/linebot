c813cceb5a1928099b4b91afb834c7fe00e743bc
Chien-Wu2/ <g0910186@hchs.hc.edu.t>
2021-07-10 21:50:43 +0800

    復原

--- app.py
+++ app.py
@@ -37,9 +37,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    if '吃飽'in msg :
+        r = '吃飽了'
     line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text=event.message.text))
+    TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,4 @@
+msg=input('輸入：')
+if '吃飽'in msg:
+    r = '吃飽了'
+    print(r)
