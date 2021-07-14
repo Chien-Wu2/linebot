45e5bf063e9095e1c0f215b72a5e8eca3af29f2a
Chien-Wu2/ <g0910186@hchs.hc.edu.t>
2021-07-14 19:59:02 +0800

    再試一次

--- app.py
+++ app.py
@@ -44,7 +44,11 @@ def handle_message(event):
         sticker_message = StickerSendMessage(
         package_id='1',
         sticker_id='1'
-        )
+    )
+    
+    line_bot_api.reply_message(
+    event.reply_token,
+    sticker_message)
 
 
     with open('answer.txt','r',encoding='utf8')as f:
