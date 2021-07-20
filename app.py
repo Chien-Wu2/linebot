78e8f30b1b610ee6c67c18a09c9c4302baeb5385
Chien-Wu2/ <g0910186@hchs.hc.edu.t>
2021-07-20 22:16:16 +0800

    sticker update

--- app.py
+++ app.py
@@ -50,13 +50,13 @@ def handle_message(event):
 
             if keyword in msg:
                 r = answer
-                
+            
                 line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text = r))
-
-                return
-                
+            else:
+                r = no   
+    if r == no:
         sticker_message = StickerSendMessage(
         package_id='11539',
         sticker_id='52114117'
